854c056c0693c80f07112c57b845a88655c73bdd
Paweł P <pp.polakiewicz@gmail.com>
2020-05-03 00:35:36 +0200

    Refactored game details download

--- src/app/game/interface/iround.ts
+++ src/app/game/interface/iround.ts
@@ -1,11 +1,10 @@
-import { IBot } from './ibot'
+import { IBot } from './ibot';
 
 export interface IRound {
-
-    ADVANTAGE: string;
-    BOT_1: IBot;
-    BOT_2: IBot;
-    ROUND: string;
-    TIME: Date;
-    WINNER: string;
+  ADVANTAGE: string;
+  BOT_1: IBot;
+  BOT_2: IBot;
+  ROUND: string;
+  TIME: Date;
+  WINNER: number;
 }
