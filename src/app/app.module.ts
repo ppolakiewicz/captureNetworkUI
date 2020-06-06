f3bc2355e64d707b45c1bdba1db28a03179b437e
Paweł P <pp.polakiewicz@gmail.com>
2020-06-07 01:11:07 +0200

    Clear imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-import { MainMenuItemComponent } from './main-menu/main-menu-item/main-menu-item.component';
+import { GameTableComponent } from './game/game-table/game-table.component';
 import { GameComponent } from './game/game.component';
-import { GameTableComponent } from './game/game-table/game-table.component'
+import { MainMenuItemComponent } from './main-menu/main-menu-item/main-menu-item.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
 
 @NgModule({
   declarations: [
@@ -37,9 +36,7 @@ import { GameTableComponent } from './game/game-table/game-table.component'
     MatIconModule,
     MatButtonModule,
     MatTableModule,
-    MatGridListModule,
-    HttpClientModule,
-    HttpClientJsonpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
